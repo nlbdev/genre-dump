2baacfc93bd01da36bb1cf49c4e629f08af427a6
Jostein Austvik Jacobsen <josteinaj@gmail.com>
2018-08-20 14:46:55 +0200

    initial version

---
+++ build-xml.py
@@ -0,0 +1,90 @@
+#!/usr/bin/env python3
+
+import os
+import sys
+import csv
+from lxml import etree as ET
+
+if sys.version_info[0] != 3:
+    print("# This script requires Python version 3.x")
+    sys.exit(1)
+
+def xml_escape(value):
+    value = "" if value == None else str(value)
+    value = value.replace("&", "&amp;").replace("<", "&lt;").replace(">","&gt;").replace("\"", "&quot;")
+    return value
+
+def main(argv):
+    if len(argv) < 3:
+        print("usage: ./run.py </path/to/genre.csv> <output_dir>")
+        sys.exit(1)
+    
+    genre_csv = argv[1]
+    out_dir = argv[2]
+    
+    if not os.path.isfile(genre_csv):
+        print("Not a file: " + str(genre_csv))
+        sys.exit(1)
+    
+    if not os.path.isdir(out_dir):
+        print("Not a directory: " + str(out_dir))
+        sys.exit(1)
+    
+    genres = {}
+    
+    genre_id = 1
+    with open(genre_csv, newline='') as csvfile:
+        csvreader = csv.reader(csvfile, delimiter=',', quotechar='"')
+        for row in csvreader:
+            genre = row[0]
+            identifier = row[1]
+            title = row[2]
+            
+            if genre is None or genre == "":
+                continue
+            
+            if genre not in genres:
+                genres[genre] = {
+                    "id": str(genre_id),
+                    "books": {}
+                }
+                genre_id += 1
+            genres[genre]["books"][identifier] = title
+    
+    genre_list_csv = os.path.join(out_dir, "genres.csv")
+    with open(genre_list_csv, 'w', newline='') as csvfile:
+        csvwriter = csv.writer(csvfile, delimiter=',', quotechar='"', quoting=csv.QUOTE_ALL)
+        for genre in genres:
+            csvwriter.writerow([genre, genres[genre]["id"]])
+    
+    for genre in genres:
+        genre_xml = os.path.join(out_dir, genres[genre]["id"] + ".xml")
+        
+        # <active_loans>...</active_loans>
+        active_loans = ET.Element("active_loans")
+        
+        # <result><success>true</success><message/></result>
+        result = ET.SubElement(active_loans, "result")
+        success = ET.SubElement(result, "success")
+        success.text = "true"
+        message = ET.SubElement(result, "message")
+        
+        # <data><books>...</books></data>
+        data = ET.SubElement(active_loans, "data")
+        data.append(ET.Comment(" " + genre + " "))
+        books = ET.SubElement(data, "books")
+        
+        for identifier in genres[genre]["books"]:
+            # <book><id>...</id><title>...</title></book>
+            book = ET.SubElement(books, "book")
+            id_element = ET.SubElement(book, "id")
+            id_element.text = identifier
+            title = ET.SubElement(book, "title")
+            title.text = genres[genre]["books"][identifier]
+
+        tree = ET.ElementTree(active_loans)
+        tree.write(genre_xml, xml_declaration=True, encoding='UTF-8', pretty_print=True)
+
+
+if __name__ == "__main__":
+    main(sys.argv)
---
+++ run.sh
@@ -0,0 +1,61 @@
+#!/bin/bash
+
+VMARC="$1"
+OUTDIR="$2"
+
+if [ "$VMARC" != "" ] && [ ! -f "$VMARC" ]; then
+    echo "'$VMARC' finnes ikke"
+fi
+if [ "$OUTDIR" != "" ] && [ ! -d "$OUTDIR" ]; then
+    echo "'$OUTDIR' finnes ikke"
+fi
+if [ ! -f "$VMARC" ] || [ ! -d "$OUTDIR" ]; then
+    echo "bruk: ./run.sh </path/to/data.vmarc.txt> <output_dir>"
+    exit 1
+fi
+
+GENRE_TEMP_MARC="/tmp/genre.marc"
+GENRE_TEMP_CSV="/tmp/genre.csv"
+
+echo "lager $GENRE_TEMP_MARC..."
+cat "$VMARC" | grep "^.\(001\|245\|655\)" > $GENRE_TEMP_MARC
+
+echo "lager $GENRE_TEMP_CSV..."
+if [ -f "$GENRE_TEMP_CSV" ]; then
+    rm "$GENRE_TEMP_CSV"
+fi
+IDENTIFIER=""
+TITLE=""
+GENRE=""
+TOTAL_BOOKS="`cat "$VMARC" | grep "^.\(001\)" | wc -l`"
+COUNT=0
+while read line; do
+    
+    TAG="`echo $line | sed 's/^.\(...\).*$/\1/'`"
+    if [ "$TAG" = "001" ]; then
+        BOOK_COUNTER=$((BOOK_COUNTER+1))
+        if [ "$(($BOOK_COUNTER % 1000))" = "0" ]; then
+            echo "$BOOK_COUNTER av $TOTAL_BOOKS katalogposter behandlet"
+        fi
+        
+        if [ "$IDENTIFIER" != "" ]; then
+            echo "\"$GENRE\",\"$IDENTIFIER\",\"$TITLE\"" >> $GENRE_TEMP_CSV
+            IDENTIFIER=""
+            TITLE=""
+            GENRE=""
+        fi
+        IDENTIFIER="`echo $line | sed 's/^....0*//' | sed 's/"/""/'`"
+    elif [ "$TAG" = "245" ]; then
+        TITLE="`echo $line | grep '\$a' | sed 's/.*\$a//' | sed 's/\$.*//' | sed 's/"/""/'`"
+    elif [ "$TAG" = "655" ]; then
+        GENRE="`echo $line | grep '\$a' | sed 's/.*\$a//' | sed 's/\$.*//' | sed 's/"/""/'`"
+    fi
+done < "$GENRE_TEMP_MARC"
+if [ "$IDENTIFIER" != "" ]; then
+    echo "\"$GENRE\",\"$IDENTIFIER\",\"$TITLE\"" >> $GENRE_TEMP_CSV
+fi
+
+echo "lager XML..."
+python3 build-xml.py $GENRE_TEMP_CSV $OUTDIR
+
+echo "Sjanger-XML ligger nå ferdig i '$OUTDIR'."
